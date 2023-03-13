3e396518eab5b89a664de34d6abd1cbb1a22de74
Nguyen Quoc Quan <n2qit1590@gmail.com>
2023-03-14 00:04:19 +0700

    edit script

--- dev/main.tf
+++ dev/main.tf
@@ -1,6 +1,6 @@
 module "ft-web" {
   source = "../modules/"
   instance_name = "tf-web-server-test"
-  startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Test terraform!</h1></body></html>' | sudo tee /var/www/html/index.html"
+  startup_script = "sudo apt-get update && sudo apt-get install apache2 git-all -y && sudo mkdir /var/www/html && cd /var/www/html && sudo git clone https://github.com/n2qit1590/capiclean.git&& sudo cp -rf capiclean/* . "
   firewall_name = "tf-web-server-fw"
 }
