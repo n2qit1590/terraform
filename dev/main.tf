dbdce0403dd9c620ec5a583539317970cc2d9698
Nguyen Quoc Quan <n2qit1590@gmail.com>
2023-05-26 14:58:31 +0700

    edit script

--- dev/main.tf
+++ dev/main.tf
@@ -1,6 +1,14 @@
 module "ft-web" {
   source = "../modules/"
   instance_name = "tf-web-server-test"
-  startup_script = "sudo apt-get update && sudo apt-get install apache2 git-all -y && sudo mkdir /var/www/html && cd /var/www/html && sudo git clone https://github.com/n2qit1590/capiclean.git&& sudo cp -rf capiclean/* . "
+  startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && sudo mkdir -p /var/www/html && cd /var/www/html && sudo git clone https://github.com/n2qit1590/capiclean.git&& sudo cp -rf capiclean/* . "
   firewall_name = "tf-web-server-fw"
 }
+
+module "vm-nodejs" {
+  source = "../modules/"
+  instance_name = "vm-nodejs-test"
+  startup_script = "sudo apt-get update && sudo apt-get install php8.0 -y"
+  firewall_name = "nodejs-fw"
+}
+
--- dev/variables.tf
+++ dev/variables.tf
@@ -1,4 +1,4 @@
 
 locals {
-  dev_gce_install = "$file("../templates/startup_script_gce.tmpl")}"
+  dev_gce_install = "${file("../templates/startup_script_gce.tmpl")}"
 }
