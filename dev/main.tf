a9b4242866f83d2f2a672f9bdf64351c783053f0
n2qit1590 <n2qit1590@gmail.com>
2024-09-29 23:10:20 +0700

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -1,7 +1,7 @@
 module "ft-web" {
   source = "../modules/"
   instance_name = "tf-web-server-test"
-  startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && sudo mkdir -p /var/www/html && cd /var/www/html && sudo git clone https://github.com/n2qit1590/capiclean.git && sudo cp -rf capiclean/* . "
+  startup_script = "sudo apt-get update && sudo apt-get install apache2 git-all -y && sudo mkdir -p /var/www/html && cd /var/www/html && sudo git clone https://github.com/n2qit1590/capiclean.git && sudo cp -rf capiclean/* . "
   firewall_name = "tf-web-server-fw"
 }
 
